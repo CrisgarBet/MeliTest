4eaa11ec3701696c166d18527c197173acb07ed2
Cristian Garcia Escobar <cristiangear@gmail.com>
2023-08-16 08:56:48 -0500

    fix: disable darkmode in app.

--- app/src/main/java/com/example/melitest/MeliTest.kt
+++ app/src/main/java/com/example/melitest/MeliTest.kt
@@ -1,7 +1,16 @@
 package com.example.melitest
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MeliTest : Application()
+class MeliTest : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+    }
+
+}
